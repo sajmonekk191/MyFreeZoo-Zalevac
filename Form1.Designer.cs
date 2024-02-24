81191e97804cdc6ad40713b343cdfa29caeb8e98
Šimon Novák <82063567+sajmonekk191@users.noreply.github.com>
2024-02-24 02:05:26 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace MyFreeZooZalevac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.ButtonSelector = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MyFreeZooZalevac.Properties.Resources.Kapka;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(87, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 33);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(144, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(29, 33);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(37, 63);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(93, 64);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(150, 64);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // ButtonSelector
+            // 
+            this.ButtonSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ButtonSelector.FormattingEnabled = true;
+            this.ButtonSelector.Items.AddRange(new object[] {
+            "Num1",
+            "Num2",
+            "Num3",
+            "Ctrl",
+            "Alt"});
+            this.ButtonSelector.Location = new System.Drawing.Point(38, 126);
+            this.ButtonSelector.Name = "ButtonSelector";
+            this.ButtonSelector.Size = new System.Drawing.Size(121, 21);
+            this.ButtonSelector.TabIndex = 7;
+            this.ButtonSelector.SelectedIndexChanged += new System.EventHandler(this.ButtonSelector_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Button";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 212);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonSelector);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ComboBox ButtonSelector;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using Hazdryx.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFreeZooZalevac
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+
+        private readonly Color PixelColor = Color.FromArgb(176, 176, 176);
+        private Keys AttackKey = Keys.NumPad1;
+        private bool Zalevat = false;
+        private bool Penize = false;
+        private bool Zvirata = false;
+        private readonly System.Threading.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new System.Threading.Timer(async _ => await Timer_TickAsync(), null, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
+        }
+
+        private async Task Timer_TickAsync()
+        {
+            if ((GetAsyncKeyState(AttackKey) & 0x8000) != 0 && Zalevat)
+            {
+                var point = FindAndClickPixel(PixelColor);
+                if (point != Point.Empty)
+                {
+                    await ClickAtPoint(point);
+                }
+            }
+        }
+
+        private Dictionary<string, Keys> keyMap = new Dictionary<string, Keys>
+        {
+            { "Num1", Keys.NumPad1 },
+            { "Num2", Keys.NumPad2 },
+            { "Num3", Keys.NumPad3 },
+            { "Ctrl", Keys.Control },
+            { "Alt", Keys.Alt }
+        };
+
+        private async Task ClickAtPoint(Point point)
+        {
+            SetCursorPos(point.X, point.Y - 3);
+            await Task.Delay(5);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, point.X, point.Y, 0, UIntPtr.Zero);
+            mouse_event(MOUSEEVENTF_LEFTUP, point.X, point.Y, 0, UIntPtr.Zero);
+        }
+
+        private Point FindAndClickPixel(Color PixelColor)
+        {
+            int searchvalueNormal = PixelColor.ToArgb();
+
+            Rectangle rect = new Rectangle(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Bitmap BMP = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppRgb);
+            using (Graphics GFX = Graphics.FromImage(BMP))
+            {
+                GFX.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size, CopyPixelOperation.SourceCopy);
+            }
+
+            using (FastBitmap bitmap = new FastBitmap(BMP))
+            {
+                for (int i = 0; i < bitmap.Length - 1; i++)
+                {
+                    int currentPixel = bitmap.GetI(i);
+                    if (currentPixel == searchvalueNormal)
+                    {
+                        return GetPointFromIndex(i, rect.Width);
+                    }
+                }
+            }
+
+            return Point.Empty;
+        }
+        private Point GetPointFromIndex(int index, int width)
+        {
+            int row = index / width;
+            int col = index % width;
+            return new Point(col, row);
+        }
+        private void ButtonSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ButtonSelector.SelectedItem != null)
+            {
+                var selectedValue = ButtonSelector.SelectedItem.ToString();
+                if (keyMap.ContainsKey(selectedValue))
+                {
+                    AttackKey = keyMap[selectedValue];
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Zalevat = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Penize = checkBox2.Checked;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            Zvirata = checkBox3.Checked;
+        }
+    }
+}
